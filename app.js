047e372853fb2ceaa85e8019b6b1eed8ca0fe78d
Anderson Lima <anderson.lima@VILA-SUL.com>
2024-06-10 10:02:22 -0300

    alterações

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
+
+
  // CONEXAO DB 
 pool.connect((err, client, release) => {
     if (err) {
@@ -103,7 +105,7 @@ app.get("/aluno/:matricula", async (req, res) => {
 
 // método get matriculas , faz uma requisição ao banco de dados , com a matricula que foi passada como input
 
-app.get("/aluno/:matricula", async (req, res) => {
+/*app.get("/aluno/:matricula", async (req, res) => {
   try {
       const matricula = req.params.matricula;
       const query = 'SELECT * FROM ALUNO WHERE MATRICULA = $1';
@@ -126,122 +128,51 @@ app.get("/aluno/:matricula", async (req, res) => {
 });
 
 
-
-app.get("/aluno/:matricula/download", async (req, res) => {
-  try {
-    const matricula = req.params.matricula;
-    const query = 'SELECT * FROM ALUNO WHERE MATRICULA = $1';
-    const result = await pool.query(query, [matricula]);
-
-    if (result.rows.length === 0) {
-      return res.status(404).send('Aluno não encontrado.');
-    }
-
-    const aluno = result.rows[0];
-
-    // Configuração do Puppeteer
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    const ejsFilePath = path.join(__dirname, 'views', 'aluno.ejs');
-
-    // Renderiza a página aluno.ejs com os dados do aluno
-    const html = await ejs.renderFile(ejsFilePath, { aluno });
-
-    // Gera o PDF a partir do HTML renderizado
-    await page.setContent(html);
-    const pdfBuffer = await page.pdf({ format: 'A4' });
-
-    // Fecha o navegador Puppeteer
-    await browser.close();
-
-    // Define os headers para o download do PDF
-    res.setHeader('Content-disposition', 'attachment; filename=aluno.pdf');
-    res.setHeader('Content-type', 'application/pdf');
-
-    // Envia o PDF como resposta
-    res.send(pdfBuffer);
-
-  } catch (error) {
-    console.error('Erro ao executar a consulta:', error);
-    res.status(500).send('Erro ao executar a consulta.');
-  }
-});
-
-
-
-
-
-
-
-
-/*app.get("/alunos/download", async (req, res) => {
-  try {
-    const query = 'SELECT * FROM ALUNO';
-    const result = await pool.query(query);
-
-    if (result.rows.length === 0) {
-      return res.status(404).send('Nenhum aluno encontrado.');
-    }
-
-    const alunos = result.rows;
-
-    // Crie um arquivo ZIP temporário
-    const zipPath = './alunos.zip';
-    const output = fs.createWriteStream(zipPath);
-    const archive = archiver('zip');
-
-    output.on('close', () => {
-      // Envie o arquivo ZIP como resposta
-      res.download(zipPath, 'alunos.zip', (err) => {
-        if (err) {
-          console.error('Erro ao enviar o arquivo ZIP:', err);
-          res.status(500).send('Erro ao enviar o arquivo ZIP.');
-        }
-        // Remova o arquivo ZIP temporário depois de enviado
-        fs.unlinkSync(zipPath);
-      });
-    });
-
-    archive.pipe(output);
-
-    // Gere PDFs para cada aluno e adicione ao arquivo ZIP
-    for (let i = 0; i < alunos.length; i++) {
-      const aluno = alunos[i];
-      const pdfPath = path.join(__dirname, `aluno_${i}.pdf`);
-
-      // Renderize o arquivo EJS
-      const renderedHTML = ejs.renderFile(path.join(__dirname, 'views', 'aluno.ejs'), { aluno })
-
-
-      // Inicie o navegador Puppeteer
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-
-      // Acesse a página renderizada e gere o PDF
-      await page.setContent(renderedHTML);
-      await page.pdf({ path: pdfPath, format: 'A4' });
-
-      // Feche o navegador Puppeteer
-      await browser.close();
-
-      // Adicione o PDF ao arquivo ZIP
-      archive.append(fs.createReadStream(pdfPath), { name: `aluno_${i}.pdf` });
-
-      // Exclua o arquivo PDF temporário
-      fs.unlinkSync(pdfPath);
-    }
-
-    // Finalize o arquivo ZIP
-    archive.finalize();
-
-  } catch (error) {
-    console.error('Erro ao executar a consulta:', error);
-    res.status(500).send('Erro ao executar a consulta.');
-  }
-});
-
-
-
 */
+
+
+// metodo para buscar aluno por nome ou matricula 
+
+app.get("/aluno/:parametro", async (req, res) => {
+  try {
+      const parametro = req.params.parametro;
+      const isMatricula = !isNaN(parametro); // Verifica se o parâmetro é uma matrícula
+
+      let query;
+      let values;
+      if (isMatricula) {
+          query = 'SELECT * FROM ALUNO WHERE MATRICULA = $1';
+          values = [parametro];
+      } else {
+          query = 'SELECT * FROM ALUNO WHERE NOME ILIKE $1'; // ILIKE para busca case-insensitive
+          values = [`%${parametro}%`];
+      }
+
+      const result = await pool.query(query, values);
+
+      if (result.rows.length === 0) {
+          return res.status(404).send('Aluno não encontrado.');
+      }
+
+      const aluno = result.rows[0];
+      
+      // Renderizar o resultado na view aluno
+      res.render('aluno', { aluno });
+
+  } catch (error) {
+      console.error('Erro ao executar a consulta:', error);
+      res.status(500).send('Erro ao executar a consulta.');
+  }
+});
+
+
+
+
+
+
+
+
+
+
 
 
